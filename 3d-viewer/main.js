24122404bce9a77d4b6f194532e011348eca731d
raihven <raihven@protonmail.com>
2023-10-09 22:26:39 -0400

    autoRotate fixed, model selection dropdown menu, new models addeed, auto rotate toggle

--- 3d-viewer/main.js
+++ 3d-viewer/main.js
@@ -1,17 +1,20 @@
 import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
-//import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import { TrackballControls } from "three/addons/controls/TrackballControls.js";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+// import { TrackballControls } from "three/addons/controls/TrackballControls.js";
+//trackball does not support .autoRotate
 
-const canvas = document.querySelector("#webgl");
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
-renderer.setSize(window.innerWidth , window.innerHeight );
-renderer.LinearEncoding = THREE.SRGBColorSpace;
+const webglCanvas = document.querySelector("#webgl");
+const renderer = new THREE.WebGLRenderer({
+  canvas: webglCanvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.LinearEncoding= THREE.SRGBColorSpace;
 
 const scene = new THREE.Scene();
 // scene.background = new THREE.Color(0xFFB5D4D6);
-// Load and set the texture
-const textureLoader = new THREE.TextureLoader();
+const textureLoader = new THREE.TextureLoader(); // Load and set the texture
 textureLoader.load("./src/assets/images/Grid.png", function (texture) {
   scene.background = texture;
 });
@@ -20,13 +23,13 @@ const camera = new THREE.PerspectiveCamera(
   45,
   window.innerWidth / window.innerHeight
 );
-camera.position.set(1, 1, 1,1);
+camera.position.set(1, 1, 1);
 
 // Stores the initial position
-const initialPosition = new THREE.Vector3(1, 1, 1,);
+const initialPosition = new THREE.Vector3(1, 1, 1);
 
 // Add event listener for middle mouse button
-canvas.addEventListener("mousedown", function (event) {
+webglCanvas.addEventListener("mousedown", function (event) {
   // Middle mouse button was clicked
   if (event.button === 1) {
     camera.position.copy(initialPosition);
@@ -34,47 +37,52 @@ canvas.addEventListener("mousedown", function (event) {
 });
 
 const loader = new GLTFLoader();
-loader.load("./src/assets/models/Wellhead.glb", function (gltf) {
-  scene.add(gltf.scene);
-});
-//     // Set the position of the light to be the same as the 3D model
-//     light1.position.copy(gltf.scene.position);
-//     light2.position.copy(gltf.scene.position);
-//     light3.position.copy(gltf.scene.position);
-//     // Attempt at setting camera position to to model
 
-// Creates a point light
-// const light1 = new THREE.PointLight( 0xfffff,20,100);
-// light1.position.set(50,30,50);
-// scene.add(light1);
-
-// const light2 = new THREE.PointLight( 0xffffff,10,100);
-// light2.position.set(-50,30,50);
-// scene.add(light2);
-
-// const light3 = new THREE.PointLight( 0xffffff,20,100);
-// light3.position.set(0,30,-5);
-// scene.add(light3);
-
-const ambientLight = new THREE.AmbientLight(0xffffff, 2.45);
-scene.add(ambientLight);
-
-const controls = new TrackballControls(camera, canvas);
-controls.mouseButtons = {
-  LEFT: THREE.MOUSE.ROTATE,
- MIDDLE: THREE.MOUSE.DOLLY,
-  RIGHT: THREE.MOUSE.PAN,
+const loadModel = () => {
+  const selectedModelPath = document.getElementById("modelSelector").value;
+  loader.load(selectedModelPath, function (gltf) {
+    // Remove the previous model from the scene if there is one
+    scene.children.forEach((child) => {
+      if (child.isMesh || child.isGroup) {
+        scene.remove(child);
+      }
+    });
+    scene.add(gltf.scene); // Add the new model to the scene
+  });
 };
 
-// Enable rotation
-// The speed at which the object rotates, in degrees per frame.
-controls.autoRotate = true;
-controls.autoRotateSpeed = -0.5; // 30 seconds per round when fps is 60
+// Initial load of model
+loadModel();
 
+// Add an event listener to the dropdown
+document.getElementById("modelSelector").addEventListener("change", loadModel);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+scene.add(ambientLight);
+
+const controls = new OrbitControls(camera, webglCanvas);
+controls.autoRotate = true;
+controls.autoRotateSpeed = 1;
+controls.enableDamping = true;
+controls.rotationSpeed = 0.5;
+controls.mouseButtons = {
+  LEFT: THREE.MOUSE.ROTATE,
+  // MIDDLE: THREE.MOUSE.DOLLY,
+  RIGHT: THREE.MOUSE.PAN,
+};
+const toggleAutoRotate = () => {
+  controls.autoRotate = !controls.autoRotate;
+};
+
+// Add an event listener to the button
+document
+  .getElementById("toggleRotation")
+  .addEventListener("click", toggleAutoRotate);
+
+// Function to update and render the scene
 function animate() {
   requestAnimationFrame(animate);
 
-  // Update the controls
   controls.update();
   renderer.render(scene, camera);
 }
